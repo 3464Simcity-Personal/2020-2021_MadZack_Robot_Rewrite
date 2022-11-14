f591020caf9f708a228527fec0d9d8335ee3aa46
Acer1 <Acer1>
2022-11-14 18:13:49 -0500

    commenting out stuff

--- test.java
+++ test.java
@@ -2,5 +2,8 @@ private final school = 1;
 System.out.print("L");
 
 
-System.out.println("Hello")
-System.out.println("This is interesting.")
+System.out.println("Hello");
+// System.out.println("This is interesting.");
+
+
+
